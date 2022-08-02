36344d6e88308610af59876483c8d454c31518e2
Jake Casto <jake@lynndigital.com>
2022-08-01 20:03:11 -0400

    feat: prettier

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,10 +1,12 @@
 import config from '../../cart.config'
 import { parse } from './cookie'
-import { decrypt } from './encrypt';
+import { decrypt } from './encrypt'
 
-export default async function useRequest(request: Request): Promise<{ host: string; cartToken: string; }> {
+export default async function useRequest(
+  request: Request,
+): Promise<{ host: string; cartToken: string }> {
   const cookie = parse(request.headers.get('Cookie') || '')
-  const cartToken = await decrypt(cookie[config.cookie.name]) ?? config.cookie.generator()
+  const cartToken = (await decrypt(cookie[config.cookie.name])) ?? config.cookie.generator()
   const host = new URL(request.url).hostname
 
   return {
